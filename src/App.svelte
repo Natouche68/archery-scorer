bad270ade6f005ddced6910810e675932786fd7f
Natouche68 <ngrossheny@gmail.com>
2023-01-09 19:06:27 +0100

    Create the UI for the home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Navbar from './lib/Navbar.svelte';
+	import Home from './lib/Home.svelte';
 </script>
 
 <Navbar />
+<Home />
---
+++ src/lib/Home.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	let volleyNumber: number;
+	let arrowNumber: number;
+</script>
+
+<form class="home">
+	<div class="title">Créer une nouvelle feuille de score</div>
+	<input type="number" bind:value={volleyNumber} placeholder="Nombre de volées" required />
+	<input type="number" bind:value={arrowNumber} placeholder="Nombre de flèches" required />
+	<button type="submit">Créer</button>
+</form>
+
+<style>
+	.home {
+		padding: 24px 5%;
+	}
+
+	.title {
+		font-weight: 600;
+		font-size: 36px;
+		margin: 0 0 16px 0;
+	}
+
+	input {
+		margin: 8px 0;
+		width: 100%;
+		font-size: 18px;
+		font-family: Poppins, Calibri, sans-serif;
+		border: none;
+		border-bottom: 1px solid var(--neutral-color);
+		color: var(--neutral-color);
+		outline: none;
+		transition: all 0.4s ease;
+	}
+
+	input::placeholder {
+		color: var(--neutral-color);
+	}
+
+	input:focus {
+		color: var(--primary-color);
+		border-bottom: 3px solid var(--primary-color);
+		margin: 7px 0;
+	}
+
+	button {
+		margin: 16px 6px;
+		padding: 6px;
+		width: calc(100% - 6px * 2);
+		font-family: Poppins, Calibri, sans-serif;
+		font-size: 20px;
+		font-weight: 500;
+		border: none;
+		border-radius: 6px;
+		background: var(--primary-color);
+		color: #fff;
+		transition: all 0.4s ease;
+		cursor: pointer;
+	}
+
+	button:hover {
+		margin: 18px 8px;
+		padding: 4px;
+		border-radius: 12px;
+		width: calc(100% - 8px * 2);
+	}
+</style>
