226ef54d2ade9fceddf7daf5c3de6337e1ed752c
Natouche68 <ngrossheny@gmail.com>
2023-01-10 19:17:20 +0100

    Create a viewer for the score sheet

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,12 @@
 	import { scoreSheet } from './lib/ScoreSheet';
 	import Navbar from './lib/Navbar.svelte';
 	import Home from './lib/Home.svelte';
+	import ScoreSheetEditor from './lib/ScoreSheetEditor.svelte';
 </script>
 
 <Navbar />
 {#if $scoreSheet}
-	{$scoreSheet.name}
+	<ScoreSheetEditor />
 {:else}
 	<Home />
 {/if}
---
+++ src/lib/ScoreSheetEditor.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { scoreSheet } from './ScoreSheet';
+
+	function calcTotalVolley(index: number): number {
+		let total: number = 0;
+
+		$scoreSheet.scoreSheet[index].forEach((value) => {
+			if (value != '-' && value != 'M') {
+				total += value;
+			}
+		});
+
+		return total;
+	}
+
+	function calcCumulTotal(index: number): number {
+		if (index == 0) {
+			return calcTotalVolley(index);
+		} else {
+			return calcTotalVolley(index) + calcCumulTotal(index - 1);
+		}
+	}
+</script>
+
+<div class="score-sheet-editor">
+	<div class="title">
+		{$scoreSheet.name}
+	</div>
+	{#each $scoreSheet.scoreSheet as volley, index}
+		<div class="volley">
+			{#each volley as score}
+				<div class="score">
+					{score + ' '}
+				</div>
+			{/each}
+			<div class="total">
+				{calcTotalVolley(index)}
+			</div>
+			<div class="total">
+				{calcCumulTotal(index)}
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.score-sheet-editor {
+		padding: 24px 5%;
+	}
+
+	.title {
+		font-weight: 600;
+		font-size: 36px;
+		margin: 0 0 16px 0;
+	}
+
+	.volley {
+		padding: 6px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		border-bottom: 1px solid var(--neutral-color);
+	}
+
+	.volley:last-child {
+		border: none;
+	}
+
+	.score {
+		width: 60px;
+		text-align: center;
+		font-size: 20px;
+	}
+
+	.total {
+		width: 60px;
+		text-align: center;
+		font-size: 20px;
+		font-weight: 500;
+	}
+</style>
